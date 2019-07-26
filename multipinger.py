4e8a52a5c889d03544f9e86a075099e4ebac77b8
odiumdev <Tuunoid@gmail.com>
2019-07-26 17:21:29 -0500

    Update multipinger.py

--- multipinger.py
+++ multipinger.py
@@ -6,7 +6,7 @@ iplist.close()
 f=open("online.txt", "a+")
 g=open("offline.txt", "a+")
 for line in lines:
-    if os.system("ping -n 1 " + line) == 0:
+    if os.system("ping -n 1 " + line) == 0: #FOR LINUX CHANGE "ping -n 1 " TO "ping -c 1 "
         f.write(line)
     else:
         g.write(line)
